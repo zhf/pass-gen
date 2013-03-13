962a423daf3ab2cef66ea2d2e588e99a3b06240e
zhf <hf@zhanghongfeng.com>
2013-03-13 18:40:34 +0800

    remove L 2 Z from chars

--- pass-gen.pl
+++ pass-gen.pl
@@ -10,9 +10,9 @@ sub randomPassword {
 	}
 
 	my @chars = split(" ",
-	"a b c d e f g h j k l m n p q r s t u v w x y z 
-	 A B C D E F G H J K L M N P Q R S T U V W X Y Z
-	 2 3 4 5 6 7 8 9 
+	"a b c d e f g h j k m n p q r s t u v w x y 
+	 A B C D E F G H J K M N P Q R S T U V W X Y 
+	 3 4 5 6 7 8 9 
 	 $ @ = + % & *");
 	my $num_chars = @chars;
 
