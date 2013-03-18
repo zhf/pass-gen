67f47901ae720678eb56b5b1c737e2a46cf2925b
zhf <hf@zhanghongfeng.com>
2013-03-18 20:54:37 +0800

    add 3 char sets for simple to strong passwords

--- pass-gen.pl
+++ pass-gen.pl
@@ -1,24 +1,35 @@
 #!/usr/bin/perl
 
+@simple_chars = split(" ",
+"a b c d e f g h j k m n p q r s t u v w x y 
+ 3 4 5 6 7 8 9");
+
+@regular_chars = split(" ",
+"a b c d e f g h j k m n p q r s t u v w x y 
+ A B C D E F G H J K M N P Q R S T U V W X Y 
+ 3 4 5 6 7 8 9");
+
+@strong_chars = split(" ",
+"a b c d e f g h j k m n p q r s t u v w x y 
+ A B C D E F G H J K M N P Q R S T U V W X Y 
+ 3 4 5 6 7 8 9 
+ $ @ = + % & *");
+
+$password_length = $ARGV[0];
+if (!$password_length) {
+	$password_length = 10;
+}
+
 sub randomPassword {
 	my $password;
 	my $_rand;
 
-	my $password_length = $_[0];
-	if (!$password_length) {
-		$password_length = 8;
-	}
-
-	my @chars = split(" ",
-	"a b c d e f g h j k m n p q r s t u v w x y 
-	 A B C D E F G H J K M N P Q R S T U V W X Y 
-	 3 4 5 6 7 8 9 
-	 $ @ = + % & *");
+	my @chars = @{$_[0]};
 	my $num_chars = @chars;
 
 	srand;
 
-	for (my $i=0; $i <= $password_length ;$i++) {
+	for (my $i=0; $i < $password_length; $i++) {
 	 $_rand = int(rand $num_chars);
 	 $password .= $chars[$_rand];
 	}
@@ -27,7 +38,7 @@ sub randomPassword {
 
 for (my $j=0; $j < 5; $j++) {
 	print "\n\t";
-	print randomPassword($ARGV[0]);
+	print randomPassword(\@simple_chars), "\t", randomPassword(\@regular_chars), "\t", randomPassword(\@strong_chars);
 	print "\n";
 }
 
